faa43f7d59d6dd47a8719a58ddb093b8a11f05fe
Gajendra34 <gajendra34940@gmail.com>
2024-05-08 00:41:59 +0530

    add new project that is todo list

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,6 @@
 import web1 from './web1.png'
 import web2 from './web2.png'
+import web3 from './web3.png'
 import FeatherIcon from 'feather-icons-react';
 
 const Projects = () => {
@@ -26,7 +27,7 @@ const Projects = () => {
                                 <div class="inner" >
                                     <div class="thumbnail" style={{ overflow: 'auto', height: '200px' }}>
                                         <a href="javascript:void(0)">
-                                            <img src={ web1 } style={{ height: '156px' }} alt="Personal Portfolio Images" />
+                                            <img src={web1} style={{ height: '156px' }} alt="Personal Portfolio Images" />
                                         </a>
                                     </div>
                                     <div class="content">
@@ -54,7 +55,35 @@ const Projects = () => {
                                 <div class="inner">
                                     <div class="thumbnail" style={{ overflow: 'auto', height: '200px' }}>
                                         <a href="javascript:void(0)">
-                                            <img src={ web2 } style={{ height: '156px' }} alt="Personal Portfolio Images" />
+                                            <img src={web2} style={{ height: '156px' }} alt="Personal Portfolio Images" />
+                                        </a>
+                                    </div>
+                                    <div class="content">
+                                        <div class="category-info">
+                                            <div class="category-list">
+                                                <a href="javascript:void(0)">UI UX Design of Website</a>
+                                            </div>
+                                            <div class="meta">
+                                                <span><a href="javascript:void(0)"><i class="feather-heart"></i></a>
+                                                    7</span>
+                                            </div>
+                                        </div>
+                                        <h4 class="title"><a href="javascript:void(0)">rVm E-commerce Website <i
+                                            class="feather-arrow-up-right"></i></a></h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- End Single Portfolio -->
+
+                    <!-- Start Single Portfolio --> */}
+                        <div data-aos="fade-up" data-aos-delay="100" data-aos-once="true"
+                            class="col-lg-6 col-xl-4 col-md-6 col-12 mt--50 mt_md--30 mt_sm--30">
+                            <div class="rn-portfolio" data-toggle="modal" data-target="#ee1">
+                                <div class="inner">
+                                    <div class="thumbnail" style={{ overflow: 'auto', height: '200px' }}>
+                                        <a href="javascript:void(0)">
+                                            <img src={web3} style={{ height: '156px' }} alt="Personal Portfolio Images" />
                                         </a>
                                     </div>
                                     <div class="content">
@@ -67,7 +96,7 @@ const Projects = () => {
                                                     5</span>
                                             </div>
                                         </div>
-                                        <h4 class="title"><a href="javascript:void(0)">rVm E-commerce Website <i
+                                        <h4 class="title"><a href="javascript:void(0)">Todo-List Website <i
                                             class="feather-arrow-up-right"></i></a></h4>
                                     </div>
                                 </div>
@@ -98,7 +127,7 @@ const Projects = () => {
                                 <div class="col-lg-6">
                                     <div class="portfolio-popup-thumbnail">
                                         <div class="image">
-                                            <img class="w-100" src={ web1 } alt="slide" />
+                                            <img class="w-100" src={web1} alt="slide" />
                                         </div>
                                     </div>
                                 </div>
@@ -119,7 +148,7 @@ const Projects = () => {
                                                 class="rn-btn">
                                                 <span>VIEW PROJECT</span>
                                                 <FeatherIcon icon="chevron-right" />
-                                                
+
                                             </a>
                                         </div>
 
@@ -149,7 +178,7 @@ const Projects = () => {
                                 <div class="col-lg-6">
                                     <div class="portfolio-popup-thumbnail">
                                         <div class="image">
-                                            <img class="w-100" src={ web2 } alt="slide" />
+                                            <img class="w-100" src={web2} alt="slide" />
                                         </div>
                                     </div>
                                 </div>
@@ -167,6 +196,54 @@ const Projects = () => {
                                                 <FeatherIcon icon="thumbs-up" />
                                             </a>
                                             <a href="https://rvmecommerce.onrender.com/" target="_blank" class="rn-btn">
+                                                <span>VIEW PROJECT</span>
+                                                <FeatherIcon icon="chevron-right" />
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                    {/* <!-- End of .text-content --> */}
+                                </div>
+                            </div>
+                            {/* <!-- End of .row Body--> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!--End Modal Portfolio area-- > */}
+
+            {/* < !--Modal Portfolio Body area Start-- > */}
+            <div class="modal fade" id="ee1" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true"><FeatherIcon icon="x" /></span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+
+                                <div class="col-lg-6">
+                                    <div class="portfolio-popup-thumbnail">
+                                        <div class="image">
+                                            <img class="w-100" src={web3} alt="slide" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <div class="text-content">
+                                        <h3>
+                                            <span>Featured - Design</span> Todo-List
+                                        </h3>
+                                        <p class="mb--30">Designed website portal of Todo-List using Tailwind CSS,ContextAPI and ReactJS.</p>
+                                        <div class="button-group mt--20">
+                                            <a href="#" class="rn-btn thumbs-icon">
+                                                <span>LIKE THIS </span>
+                                                <FeatherIcon icon="thumbs-up" />
+                                            </a>
+                                            <a href="https://todo-list-pi-ten-20.vercel.app/" target="_blank" class="rn-btn">
                                                 <span>VIEW PROJECT</span>
                                                 <FeatherIcon icon="chevron-right" />
                                             </a>
